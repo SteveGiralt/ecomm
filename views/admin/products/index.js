211f28285e19a00ed4c00012c8f04dc99d55786f
Steve Giralt <steve.giralt@gmail.com>
2022-01-01 23:47:07 -0700

    wrap up product listing template

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -1,4 +1,5 @@
 const layout = require("../layout");
+
 module.exports = ({ products }) => {
   const renderedProducts = products
     .map((product) => {
@@ -7,19 +8,45 @@ module.exports = ({ products }) => {
         currency: "USD",
       });
       return `
-        <li>${product.title} - ${formattedPrice}
-        `;
+      <tr>
+        <td>${product.title}</td>
+        <td>${formattedPrice}</td>
+        <td>
+          <a href="/admin/products/${product.id}/edit">
+            <button class="button is-link">
+              Edit
+            </button>
+          </a>
+        </td>
+        <td>
+          <form method="POST" action="/admin/products/${product.id}/delete">
+            <button class="button is-danger">Delete</button>
+          </form>
+        </td>
+      </tr>
+    `;
     })
     .join("");
 
   return layout({
     content: `
-        <h1 class="title">Products</h1>
-        <div>
-            <ul>
-                ${renderedProducts}
-            </ul>
-        </div>
-        `,
+      <div class="control">
+        <h1 class="subtitle">Products</h1>  
+        <a href="/admin/products/new" class="button is-primary">New Product</a>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderedProducts}
+        </tbody>
+      </table>
+    `,
   });
 };
